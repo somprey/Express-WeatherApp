e28034ff98a9a551625f71ac5c45190f5ffa7923
darneltaphil <darneltaphil@gmail.com>
2020-10-29 22:02:08 +0000

    all features functional

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const request = require('request');
 require('dotenv').config();
+const cors = require('cors');
 
 const port = process.env.PORT
 const app = express(); 
 
-app.get('/getWeather/:location', (req, res) => {
+app.use(cors());
+
+app.use( (req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  })
+ 
+
+app.post('/getWeather/:location', (req, res) => {
     const location = req.params.location
    // console.log(process.env.API_URL+location+'&units='+process.env.API_UNIT+'&APPID='+process.env.API_KEY);
     request(process.env.API_URL+location+'&units='+process.env.API_UNIT+'&APPID='+process.env.API_KEY, 
